b5583025b6a2df15cb2dda0c559e319c1f43740f
Eric Doughty-Papassideris <eric.doughty@gmail.com>
2011-04-22 23:22:34 +0200

    qspkg building template

---
+++ templates/packager/init.rb
@@ -0,0 +1,16 @@
+$:.unshift(File.expand_path(File.join(File.dirname(__FILE__), '.')))
+
+require 'shellwords'
+
+App.register do
+  def run_template data
+    dicos = data[:bundles].map do |e|
+      @logger = App.require_one :logger
+      @writer = App.require_one :output_writer
+      @writer.create_output_file("#{e.id}.#{e["CFBundleVersion"]}.qspkg") do |output_filename|
+        res = `/usr/bin/ditto -z --keepParent -rsrc -c #{Shellwords.shellescape(e.info.path.to_s)} #{Shellwords.shellescape(output_filename.to_s)} 2>&1`
+        @logger.warn "Ditto output (#{$?.exitstatus})", res unless res.length == 0 && $?.exitstatus.zero?
+      end
+    end
+  end
+end
