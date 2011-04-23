321bd6734b09aba850dd70c5b70cf6ba2850653c
Eric Doughty-Papassideris <eric.doughty@gmail.com>
2011-04-23 17:39:13 +0200

    Fixed old plist template

--- templates/plist/init.rb
+++ templates/plist/init.rb
@@ -1,17 +1,19 @@
 
 App.register do
   def run_template data
-    INCLUDE_KEY_IN_PLIST = %w[CFBundleIdentifier QSModifiedDate CFBundleName CFBundleVersion QSPlugIn QSRequirements CFBundleShortVersionString]
-    dicos = data[:bundles].map do |e|
+    include_key_in_plist = %w[CFBundleIdentifier QSModifiedDate CFBundleName CFBundleVersion QSPlugIn QSRequirements CFBundleShortVersionString]
+    dicos = data[:bundles].map do |plugin|
       result = {}
-      e.each_pair do |k,v|
-        result[k] = v if INCLUDE_KEY_IN_PLIST.index(k)
+      include_key_in_plist.each do |key|
+        value = plugin[key]
+        result[key] = value if value
       end
-      result
     end
     # maybe add a filter for compatible QS version at this point ?
     dicos = { :plugins => dicos }
     dicos[:fullIndex] = true # Remove for incremental updates
-    app.write_to_output(data[:config][:plist], dicos.to_plist)
+    App.require_one(:output_writer).write_to_output(data[:config][:plist]) do |f|
+      f.write(dicos.to_plist)
+    end
   end
 end
