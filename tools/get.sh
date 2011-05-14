df4f28d2f3ada931a49067943bf61dc7e47a7d32
Eric Doughty-Papassideris <eric.doughty@gmail.com>
2011-05-14 20:42:41 +0200

    Plugin wiki scraper

--- tools/get.sh
+++ tools/get.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 if [[ -d .git ]]; then
-  ruby -rubygems <<RUBY > data/plugin_reference.txt
+  ruby -rubygems <<RUBY
   require 'media_wiki'
-  puts MediaWiki::Gateway.new("http://qsapp.com/w/api.php").get("Plugin_Reference")
+  def get(page)
+    @gw ||= MediaWiki::Gateway.new("http://qsapp.com/w/api.php")
+    text = @gw.get(page)
+    File.open("data/#{page.gsub(" ", "_")}.txt", "w") { |file| file.write(text) }
+    text
+  end
+  ref = get("Plugin_Reference")
+  ref.scan(/\[\[([^\]]+)\]\]/).map(&:first).uniq.sort.each {|f| get(f) }
 RUBY
   curl 'https://gist.github.com/raw/938822/a15edd6aa81459a32dfb525fc6651b8e826e2215/gistfile1.txt' > data/repo_locations.txt
 else
