8818048867fbad710fd2834184c1044ceea2adb4
Eric Doughty-Papassideris <eric.doughty@gmail.com>
2011-05-24 10:33:31 +0200

    Plist generation fixes

--- templates/plist.rb
+++ templates/plist.rb
@@ -1,16 +1,20 @@
+require "plist"
 App.register do
+  INCLUDE_KEY_IN_PLIST = %w[CFBundleIdentifier CFBundleName QSPlugIn QSRequirements QSPluginVersions]
+  def build_latest_update_xml data
+    data[:bundles].map do |plugin|
+      result = {}
+      INCLUDE_KEY_IN_PLIST.each do |key|
+        value = plugin[key]
+        result[key] = value if value
+      end
+      result
+    end.sort { |a, b| a['CFBundleIdentifier'] <=> b['CFBundleIdentifier'] }
+  end
   def run_template data
-    include_key_in_plist = %w[CFBundleIdentifier CFBundleName QSPlugIn QSRequirements QSPluginVersions]
     @logger = App.require_one :logger
     @logger.info "Writing update plist files" do
-      dicos = data[:bundles].map do |plugin|
-        result = {}
-        include_key_in_plist.each do |key|
-          value = plugin[key]
-          result[key] = value if value
-        end
-      end
-      # maybe add a filter for compatible QS version at this point ?
+      dicos = build_latest_update_xml(data)
       dicos = { :plugins => dicos }
       dicos[:fullIndex] = true # Remove for incremental updates
       App.require_one(:output_writer).write_to_output(data[:config][:plist]) do |f|
